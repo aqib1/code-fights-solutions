f85452bfc62b74c3f073ff77379016232670bc26
Aqib Javed <aqibbutt3078@gmail.com>
2019-07-25 05:54:04 +0500

    IPV4Address validator code commit

--- Codfights101/src/IPV4AddressValidator.java
+++ Codfights101/src/IPV4AddressValidator.java
@@ -1,12 +1,29 @@
 
 public class IPV4AddressValidator {
-	
-	boolean isIPv4Address(String inputString) {
 
-		return false;
+	static boolean isIPv4Address(String inputString) {
+		if (inputString == null || inputString.isEmpty())
+			return false;
+		if (!inputString.contains("."))
+			return false;
+		String[] components = inputString.split("\\.");
+		if (components.length != 4)
+			return false;
+		for (String sub : components) {
+			try {
+				int val = Integer.parseInt(sub);
+				if (val < 0 || val > 255)
+					return false;
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		}
+		return true;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(isIPv4Address("0.254.255.0"));
 
 	}
 
